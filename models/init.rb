456a761b65f4abd06a90c90fa4da3c3fe3bcc652
Christopher Nguyen <christopher.nguyen@gmail.com>
2013-08-15 08:56:12 -0700

    Added json requirement, active record connection and basic routing for person

--- models/init.rb
+++ models/init.rb
@@ -1,2 +1,7 @@
 require 'active_record'
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'db/development.sqlite3'
+)
 require_relative 'person'
+
--- routes/person.rb
+++ routes/person.rb
@@ -1,4 +1,20 @@
 class Irb < Sinatra::Application
+  get '/person' do
+    response.headers['Content-Type'] = 'application/json;chartset=utf-8'
+    person = Person.all.to_json
+  end
+
   get '/person/:id' do
+    person = Person.find_by_id(params[:id]).to_json
+  end
+
+  post '/person/create' do
+  end
+
+  put '/person/edit' do
+  end
+
+  delete '/person/delete/:id' do
+    
   end
 end
