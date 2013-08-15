5df21f0ca458310213ca426e81166230a48b2305
Christopher Nguyen <christopher.nguyen@gmail.com>
2013-08-15 13:39:29 -0700

    Added better seeds file and set up routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 require_relative '../models/init'
+firstNames = %w{chris ray trevor robert kevin amy joe darryl renee annie travis bryan}
+lastNames  = %w{jones tran nguyen brown white cooper smith banks kimble howser harris}
 
-Person.create!(
-  :id => 1,
-  :first_name => "Christopher",
-  :middle_name => "Lam",
-  :last_name => "Nguyen",
-  :gender => "m",
-  :email => "christoph.nguyen@gmail.com"
-)
+100.times do |i|
+  firstName = firstNames.sample
+  middleName = firstNames.sample
+  lastName = lastNames.sample
+
+  Person.create!(
+    :id => i + 1,
+    :first_name => firstName,
+    :middle_name => middleName,
+    :last_name => lastName,
+    :gender => ['m', 'f'].sample,
+    :email => firstName + "." + lastName + "@evisions.com"
+  )
+end
--- routes/person.rb
+++ routes/person.rb
@@ -5,16 +5,35 @@ class Irb < Sinatra::Application
   end
 
   get '/person/:id' do
+    response.headers['Content-Type'] = 'application/json;chartset=utf-8'
     person = Person.find_by_id(params[:id]).to_json
   end
 
   post '/person/create' do
+    Person.create! do |p|
+      p.first_name = params[:first_name]
+      p.middle_name = params[:middle_name]
+      p.last_name = params[:last_name]
+      p.gender = params[:gender]
+      p.email = params[:email]
+    end
   end
 
-  put '/person/edit' do
+  put '/person/update' do
+    person = Person.find_by_id params[:id]
+
+    Person.update_attributes do |p|
+      p.first_name = params[:first_name]
+      p.middle_name = params[:middle_name]
+      p.last_name = params[:last_name]
+      p.gender = params[:gender]
+      p.email = params[:email]
+    end
   end
 
   delete '/person/delete/:id' do
-    
+    person = Person.find_by_id params[:id]
+
+    person.destroy if !person.nil?
   end
 end
