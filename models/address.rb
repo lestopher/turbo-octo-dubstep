9c11e35794cb440bf368b082a5c2dc2ee30b7f4f
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-08-20 00:40:38 -0700

    Changed the association from HABTM to has_many and belongs_to

--- models/address.rb
+++ models/address.rb
@@ -1,4 +1,4 @@
 class Address < ActiveRecord::Base
-  has_and_belongs_to_many :person
+  belongs_to :person
   attr_accessible :street_line_1, :street_line_2, :zip_code, :city, :state, :country, :person_id
 end
--- models/person.rb
+++ models/person.rb
@@ -1,4 +1,4 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-  attr_accessible :first_name, :middle_name, :last_name, :gender, :email
+  attr_accessible :first_name, :middle_name, :last_name, :gender, :email, :person_id
 end
